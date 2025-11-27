fa3075a8a76769e359de56ad84bcc39cc8b8d82d
tharun892 <tharun1si22cs187@gmail.com>
2025-11-27 20:58:55 +0530

    2nd program

--- XSS_Project/home/views.py
+++ XSS_Project/home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def nameRef(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        if not name:
+            return HttpResponse("Please provide a name.")
+        
+        name1=name.lower()
+        if "script" in name1:
+            name2=name1.replace("script","")
+            return HttpResponse(f"Hello {name2}! Alert('Hacked')")
+        else:
+            return HttpResponse(f"Hello {name}!")
