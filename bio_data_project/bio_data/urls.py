b38a810f205d9b1907394793c1e04a8c4abfcede
tharun892 <tharun1si22cs187@gmail.com>
2025-11-28 11:42:38 +0530

    3rd lab program

--- bio_data_project/bio_data/urls.py
+++ bio_data_project/bio_data/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.bio_data_form, name="bio_data_form"),
-    path('/display',views.display_biodata,name="display_biodata"),
+    path('',views.bio_data_form,name='bio_data_form'),
+    path('display_bio_data/',views.display_bio_data,name='display_bio_data'),
 ]
--- bio_data_project/bio_data/views.py
+++ bio_data_project/bio_data/views.py
@@ -2,21 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def bio_data_form(request):
-    return render(request,'index.html')
+    return render(request, 'bio_data_form.html')
 
-def display_biodata(request):
-    if request.method=="POST":
+def display_bio_data(request):
+    if(request.method == 'POST'):
         name=request.POST.get("name")
-        age=request.POST.get('age')
-        email=request.POST.get('email')
-        address=request.POST.get('address')
+        age=request.POST.get("age")
+        phone=request.POST.get("phone")
+        email=request.POST.get("email")
+        address=request.POST.get("address")
         context={
-            "name":name,
-            "age":age,
-            "email": email,
-            "address":address,
+            'name':name,
+            'age':age,
+            'phone':phone,
+            'email':email,
+            'address':address
         }
-        return render(request,'display_biodata.html',context)
+        return render(request,'display_bio_data.html',context)
     else:
-        return HttpResponse("Invalid request")
-
+        return HttpResponse("Invalid Method")
